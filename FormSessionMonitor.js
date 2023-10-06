c109b3ac33a536e798bbb2d8eb542996c769e20e
whitwhoa <jason@youtorque.com>
2023-10-06 15:41:17 -0500

    FormSessionMonitor.js working as expected, tested

--- FormSessionMonitor.js
+++ FormSessionMonitor.js
@@ -12,6 +12,7 @@ class FormSessionMonitor
         this.isAuthenticated = null;
         this.hasRid = null;
         this.sessionExpirationTime = null;
+        this.csrfUpdateAttempted = false;
 
         this.getSessionMetaData();
 
@@ -52,24 +53,109 @@ class FormSessionMonitor
 
     monitorSessionTimeout()
     {
-        console.log('yeet001');
+
         let formSessionMonitorInstance = this;
         setInterval(() => {
 
             let expiryTimeInMs = this.sessionExpirationTime * 1000;
 
             const currentTimeInMs = Date.now();
-            const fiveMinutesInMs = 18 * 60 * 1000;
+            const fiveMinutesInMs = 5 * 60 * 1000;
+            //const fiveMinutesInMs = 19 * 60 * 1000;
 
             if ((expiryTimeInMs - currentTimeInMs) <= fiveMinutesInMs) {
-                console.log('here001');
                 this.alertMessage.show('Your session is about to expire. To continue without interruption please click the "Ok" button', 'warning', ()=>{
-                    console.log('Button Clicked: ' + formSessionMonitorInstance.sessionExpirationTime);
+                    formSessionMonitorInstance.keepSessionAlive();
                 });
             }
 
+            if(expiryTimeInMs <= currentTimeInMs)
+                location.href = '/auth/login';
+
         }, 60000);
     }
+
+    keepSessionAlive()
+    {
+        fetch("/session-meta-data", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Network response was not ok");
+            } else {
+                return response.json();
+            }
+        })
+        .then(data => {
+
+            this.hasSession = data.hasSession;
+            this.isAuthenticated = data.isAuthenticated;
+            this.hasRid = data.hasRid;
+            this.sessionExpirationTime = data.sessionExpirationTime;
+
+        })
+        .catch(error => {
+            document.querySelector('alert-message').show('An unexpected error has occurred while processing your request', 'danger', () => {});
+            console.error('There was a problem with the fetch operation:', error.message);
+        });
+    }
+
+    updateCsrf(formElementName, formObj)
+    {
+        //alert(formElementName);
+
+        if(this.csrfUpdateAttempted)
+        {
+            // should never need to make call to revalidate csrf more than once. If so, something bad has happened, so display an error message
+            // and stop making api calls
+            document.querySelector('alert-message').show('An unexpected error has occurred while processing your request22', 'danger', () => {});
+        }
+        else
+        {
+            fetch("/get-csrf", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            })
+            .then(response => {
+                if(!response.ok){
+                    throw new Error("Network response was not ok");
+                } else {
+                    return response.json();
+                }
+            })
+            .then(data => {
+
+                let csrfElement = document.querySelector('input[name="' + formElementName + '"]');
+                csrfElement.value = data.csrf;
+                this.csrfUpdateAttempted = true;
+
+                let event = new Event("submit", {
+                    'bubbles'    : true,
+                    'cancelable' : true
+                });
+
+                formObj.dispatchEvent(event);
+
+            })
+            .catch(error => {
+                document.querySelector('alert-message').show('An unexpected error has occurred while processing your request33', 'danger', () => {});
+                console.error('There was a problem with the fetch operation:', error.message);
+            });
+        }
+
+
+
+    }
+
+
 
 }
 
