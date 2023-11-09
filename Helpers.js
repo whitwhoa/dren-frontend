0a4db75c978425d959baba52a922e4cba6f4901d
whitwhoa <jason@youtorque.com>
2023-11-09 17:10:58 -0600

    -

--- Helpers.js
+++ Helpers.js
@@ -4,6 +4,13 @@ class Helpers
     {
         return window.location.pathname.split('/').filter(segment => segment !== "");
     }
+
+    static getKindaUniqueId()
+    {
+        const timestamp = new Date().getTime();
+        const randomFactor = Math.random().toString(36).substring(2, 15);
+        return `${timestamp}${randomFactor}`;
+    }
 }
 
 export default Helpers;
