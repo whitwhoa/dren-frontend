54f33d1c8df9241ae894786ec86f56fd50450c85
whitwhoa <jason@youtorque.com>
2023-12-29 13:24:52 -0600

    added function to get query string parameters which we will need in the future

--- Helpers.js
+++ Helpers.js
@@ -11,6 +11,18 @@ class Helpers
         const randomFactor = Math.random().toString(36).substring(2, 15);
         return `${timestamp}${randomFactor}`;
     }
+
+    static getQueryParams() {
+        const queryParams = {};
+        const urlObj = new URL(window.location.href);
+        const params = new URLSearchParams(urlObj.search);
+
+        for (const [key, value] of params.entries()) {
+            queryParams[key] = value;
+        }
+
+        return queryParams;
+    }
 }
 
 export default Helpers;
